298b51e8be8f5dde63e67bca0557caafa6ccb215
Marat <markbass82@gmail.com>
2020-10-05 16:49:34 +0300

    'Created reducer and Datalayer'

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect } from 'react';
 import './App.css';
 import Login from "./Components/Login/Login";
 import {getTokenFromUrl} from "./spotify";
+import SpotifyWebApi from "spotify-web-api-js"
+import Player from "./Components/Player/Player";
+import {useDataLayerValue} from "./DataLayer";
+import {SET_TOKEN, SET_USER} from "./types";
+
+const spotify = new SpotifyWebApi()
 
 function App() {
-    const [token, setToken] = useState(null)
-    //Run code based on a given condition
+    const [{ user, token }, dispatch] = useDataLayerValue()
+
     useEffect(() => {
         const hash = getTokenFromUrl()
         window.location.hash = ""
         const _token = hash.access_token;
         if(_token) {
-            setToken(_token)
+            dispatch({
+                type: SET_TOKEN,
+                token: _token
+            })
+            spotify.setAccessToken(_token)
+            spotify.getMe().then((user) => {
+                dispatch({
+                    type: SET_USER,
+                    user
+                })
+            })
         }
-        console.log('I have a token: ', token)
+        // eslint-disable-next-line
     }, [])
 
-  return (
-    <div className="app">
-        {
-            token ? (
-                <h1>I am Logged in!</h1>
-                ) : (
-                <Login />
-                )
-        }
-    </div>
-  );
+    return (
+      <div className="app">
+        {token ? <Player spotify={spotify} /> : <Login />}
+      </div>
+    );
 }
 
 export default App;
---
+++ src/reducer.js
@@ -0,0 +1,30 @@
+import {SET_TOKEN, SET_USER} from "./types"
+export const initialState = {
+    user: null,
+    playlists: [],
+    playing: false,
+    item: null,
+    //После разработки приложения, токен установить в ноль(null)
+    //REMOVE after finished developing
+    token: 'BQCnPktlhcCU2i4cVBRgaZKlyRjYyErmRWh6TJegI4XWra9oiZqxc9ddjAwuOFkTI8CLCLfoD8Y81JGUiSmTfJ4Ox6O2_d0QTFFb4phpYCnz6tGQcN3fuGtyB5G4SBl0ubGMFVjQrC_zX1KcndIkpzMBzqzYAwzEN9QE32GUSh_hj_v7',
+}
+
+const reducer = (state, action) => {
+    console.log(action)
+    switch(action.type) {
+        case SET_USER:
+            return {
+                ...state,
+                user: action.user
+            }
+        case SET_TOKEN:
+            return {
+                ...state,
+                token: action.token
+            }
+        default:
+           return state
+    }
+}
+
+export default reducer
